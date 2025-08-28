02d1db43bd1b22ece5d385e9825f291741fcff5a
Pranay Chandurkar <pranaychandurkar84@gmail.com>
2025-08-28 19:37:02 +0530

    Update Login and Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,33 +2,43 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div className='flex flex-col justify-center bg-zinc-900 text-white '>
-        <h1>Register</h1>
-        <div className='flex flex-col p-7 w-full h-screen '>
-            <form action="" 
-                className='flex items-center flex-col'
-            >
-                <input 
-                    className='block text-md px-3 py-2 border-2 border-zinc-600 mb-3 rounded-md w-full focus:ring-0'
-                    type="text"
-                    placeholder='name' 
-                />
-                <input 
-                    className="block text-md px-3 py-2 border-2 border-zinc-600 mb-3 rounded-md w-full focus:ring-0"
-                    type="email"
-                    placeholder='email' 
-                />
-                <input 
-                    className='block text-md px-3 py-2 border-2 border-zinc-600 mb-3 rounded-md w-full focus:ring-0'
-                    type="password"
-                    placeholder='password' 
-                />
-                <input 
-                    className='bg-blue-500 px-3 py-2 rounded-md w-full'
-                    type="submit" 
-                    value={"Create Admin"} 
-                />
-            </form>
+    <div className='flex flex-col p-5 justify-center h-dvh'>
+        <div className='p-5 border-2 border-zinc-200 flex flex-col gap-5 rounded-2xl'>
+            
+            <div className='flex flex-col justify-center py-1/2 w-full my-10'>
+                <form action="" 
+                    className=''
+                >
+                    <h1 className='text-4xl mb-5 font-medium'>Register</h1>
+
+                    <input 
+                        className="block text-lg px-4 py-2 border-2 border-zinc-400 mb-3 rounded-md w-full"
+                        type="text"
+                        required
+                        placeholder='name' 
+                    />
+
+                    <input 
+                        className="block text-lg px-4 py-2 border-2 border-zinc-400 mb-3 rounded-md w-full"
+                        type="email"
+                        required
+                        placeholder='email' 
+                    />
+
+                    <input 
+                        className='block text-lg px-4 py-2 border-2 border-zinc-400 mb-3 rounded-md w-full'
+                        type="password"
+                        required
+                        placeholder='password' 
+                    />
+
+                    <input 
+                        className='bg-blue-500 text-lg font-semibold px-4 py-2 rounded-md w-full text-white'
+                        type="submit" 
+                        value={"Register"} 
+                    />
+                </form>
+            </div>     
         </div>
     </div>
   )
